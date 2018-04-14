208d19dc4552db74afd38af06af95fe37acc5a53
Patrick Graef <patrick@Patricks-Mini.fritz.box>
2018-04-14 10:34:59 +0200

    application moved to src folder

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ var app     = express();
 var path    = require('path');
 
 app.use('/static', express.static(__dirname + '/static', {extensions: ['html', 'js']}));
+app.use('/src', express.static(__dirname + '/src', {extensions: ['html', 'js']}));
 app.get('/', function(req, res) {
     res.sendFile(path.join(__dirname + '/index.html'));
 });
