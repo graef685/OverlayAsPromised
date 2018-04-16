923f77f73163bddee68a1d1397a579fb260aa181
graef685 <graef685@googlemail.com>
2018-04-16 09:10:28 +0200

    Update OverlayAsPromised.js

--- src/OverlayAsPromised.js
+++ src/OverlayAsPromised.js
@@ -5,26 +5,24 @@ var OverlayAsPromised = {
         closable:       true,
         startingDelay:  0,
         minDisplayTime: 0,
-        maxDisplayTime: 0,
-        onOpen:         function() {},
-        onClose:        function() {}
+        maxDisplayTime: 0
     },
 
-    create: function(config) {
-        this.init(config);
+    create: function(config, onOpen, onClose) {
+        this.init(config, onOpen, onClose);
 
         return Object.create(this.overlay);
     },
 
-    init: function(config) {
+    init: function(config, onOpen, onClose) {
         this.config.selector       = config.selector != null ? config.selector : this.config.selector;
         this.config.permanent      = config.permanent != null ? config.permanent : this.config.permanent;
         this.config.closable       = config.closable != null ? config.closable : this.config.closable;
         this.config.startingDelay  = config.startingDelay != null ? config.startingDelay : this.config.startingDelay;
         this.config.minDisplayTime = config.minDisplayTime != null ? config.minDisplayTime : this.config.minDisplayTime;
         this.config.maxDisplayTime = config.maxDisplayTime != null ? config.maxDisplayTime : this.config.maxDisplayTime;
-        this.config.onOpen         = config.onOpen != null ? config.onOpen : this.config.onOpen;
-        this.config.onClose        = config.onClose != null ? config.onClose : this.config.onClose;
+        this.overlay.onOpen        = onOpen != null ? onOpen : this.overlay.onOpen;
+        this.overlay.onClose       = onClose != null ? onClose : this.overlay.onClose;
     },
 
     overlay: {
@@ -42,13 +40,17 @@ var OverlayAsPromised = {
                     overlayClose: OverlayAsPromised.config.closable,
                     escKey:		  OverlayAsPromised.config.closable,
                     fixed:        true,
+                    onOpen:       function () {
+                        return $(OverlayAsPromised.config.selector).show();
+                    },
+                    onClose: function () {
+                        return $(OverlayAsPromised.config.selector).hide();
+                    }
                 });
 
-
-
-                setTimeout(function() {}, OverlayAsPromised.config.startingDelay + OverlayAsPromised.config.minDisplayTime);
-
-                OverlayAsPromised.config.onOpen();
+                setTimeout(function() {
+                    OverlayAsPromised.overlay.onOpen();
+                }, OverlayAsPromised.config.startingDelay + OverlayAsPromised.config.minDisplayTime);
 
                 if(OverlayAsPromised.config.maxDisplayTime > 0 && OverlayAsPromised.config.permanent === false) {
                     setInterval(function() {
@@ -66,9 +68,17 @@ var OverlayAsPromised = {
 
         close: function() {
             $.colorbox.close();
-            OverlayAsPromised.config.onClose()
+            OverlayAsPromised.overlay.onClose();
 
             return $.when();
+        },
+
+        onOpen: function() {
+
+        },
+
+        onClose: function() {
+
         }
     }
-}
+};
